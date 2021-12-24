8abd72b42d75f19d9b87149fd01f510cb3c04468
Justin Large <justinlarge1974@gmail.com>
2021-12-24 14:04:15 -0600

    Got it to print everything I want

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const axios = require('axios');
+import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import axios from 'axios';
+import chalk from 'chalk';
+
+const cyan = chalk.cyan;
+const yellow = chalk.yellow;
+const magenta = chalk.magenta;
+const green = chalk.green;
+const red = chalk.red;
 
 const HOST = "localhost";
 const PORT = 3000;
@@ -9,11 +16,27 @@ const API_SERVICE_URL = "http://localhost:3001/";
 
 const app = express();
 
+let logMessage = '';
+let start;
+
+const formatJson = jsonString => {
+    const obj = JSON.parse(jsonString);
+    return JSON.stringify(obj, null, 2);
+}
+
+const statusColor = httpStatus => {
+    return httpStatus < 400 ? green(httpStatus) : red(httpStatus);
+}
+
 app.use(createProxyMiddleware({
     target: API_SERVICE_URL,
     changeOrigin: true,
 
     onProxyReq: (proxyReq, req, res) => {
+        logMessage += `${cyan("Request: ")} ${yellow(req.method)} ${req.url}\n\n`;
+        
+        logMessage += `${magenta('Request Headers:')}\n${JSON.stringify(req.headers, null, 2)}\n\n`
+
         let body = '';
 
         req.on('data', data => {
@@ -22,11 +45,17 @@ app.use(createProxyMiddleware({
         });
 
         req.on('end', () => {
-            console.log(`Request Body:\n${body}`);
+            logMessage += `${magenta('Request Body:')}\n${formatJson(body)}\n\n`;
+            start = process.hrtime();
         });
     },
 
     onProxyRes: (proxyRes, req, res) => {
+        const end = process.hrtime(start);
+        logMessage += `${cyan('Response:')} ${statusColor(res.statusCode)} `;
+        logMessage += `${end[0]}s ${end[1] / 1000000}ms\n\n`;
+        logMessage += `${magenta('Response Headers:')}\n${JSON.stringify(res.getHeaders(), null, 2)}\n\n`
+        
         let body = '';
 
         proxyRes.on('data', data => {
@@ -35,7 +64,10 @@ app.use(createProxyMiddleware({
         });
 
         proxyRes.on('end', () => {
-            console.log(`Response Body:\n${body}`);
+            logMessage += `${magenta('Response Body:')}\n${formatJson(body)}\n\n`;
+            logMessage += '=======================================================================';
+            console.log(logMessage);
+            logMessage = '';
         });
     }
 }));
