5b11e5221550a2cd4a65a48158fad711d347d67a
Justin Large <justinlarge1974@gmail.com>
2021-12-26 06:48:52 -0600

    Removed some troubleshooting code

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ import { v4 as uuid } from 'uuid';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
-console.log(JSON.stringify(process.env));
-
 const cyan = chalk.cyan;
 const yellow = chalk.yellow;
 const magenta = chalk.magenta;
